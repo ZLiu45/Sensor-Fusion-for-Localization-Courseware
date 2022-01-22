549a94a81a96810ac46c4ec2c12e5ace6181d795
zl-ddlabs <zhengmao.liu@doordash.com>
2022-01-21 20:21:12 -0800

    format

--- workspace/assignments/05-imu-calib/src/imu_tk/src/io_utils.cpp
+++ workspace/assignments/05-imu-calib/src/imu_tk/src/io_utils.cpp
@@ -28,53 +28,47 @@
 
 #include "imu_tk/io_utils.h"
 
-#include <iostream>
-#include <fstream>
-#include <string>
 #include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <string>
 
 // TODO Add setlocale()
 
 using namespace std;
 
 template <typename _T>
-void imu_tk::importAsciiData ( const char *filename,
-                               vector< TriadData_<_T> > &samples,
-                               TimestampUnit unit, DatasetType type )
-{
+void imu_tk::importAsciiData(const char *filename,
+                             vector<TriadData_<_T>> &samples,
+                             TimestampUnit unit, DatasetType type) {
   samples.clear();
 
   string line;
   ifstream infile;
   double ts, d[3];
 
-  infile.open ( filename );
-  if ( infile.is_open() )
-  {
+  infile.open(filename);
+  if (infile.is_open()) {
     char format[266];
-    switch ( type )
-    {
+    switch (type) {
     case DATASET_COMMA_SEPARATED:
-      sprintf ( format,"%%lf, %%lf, %%lf, %%lf" );
+      sprintf(format, "%%lf, %%lf, %%lf, %%lf");
       break;
     case DATASET_SPACE_SEPARATED:
     default:
-      sprintf ( format,"%%lf %%lf %%lf %%lf" );
+      sprintf(format, "%%lf %%lf %%lf %%lf");
       break;
     }
 
     int l = 0;
-    while ( getline ( infile,line ) )
-    {
-      int res = sscanf ( line.data(), format, &ts, &d[0], &d[1], &d[2] );
-      if ( res != 4 )
-      {
-        cout<<"importAsciiData(): error importing data in line "<<l<<", exit"<<endl;
-      }
-      else
-      {
+    while (getline(infile, line)) {
+      int res = sscanf(line.data(), format, &ts, &d[0], &d[1], &d[2]);
+      if (res != 4) {
+        cout << "importAsciiData(): error importing data in line " << l
+             << ", exit" << endl;
+      } else {
         ts /= unit;
-        samples.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[0] ), _T ( d[1] ), _T ( d[2] ) ) );
+        samples.push_back(TriadData_<_T>(_T(ts), _T(d[0]), _T(d[1]), _T(d[2])));
       }
       l++;
     }
@@ -83,11 +77,10 @@ void imu_tk::importAsciiData ( const char *filename,
 }
 
 template <typename _T>
-void imu_tk::importAsciiData ( const char *filename,
-                               vector< TriadData_<_T> > &samples0,
-                               vector< TriadData_<_T> > &samples1,
-                               TimestampUnit unit, DatasetType type )
-{
+void imu_tk::importAsciiData(const char *filename,
+                             vector<TriadData_<_T>> &samples0,
+                             vector<TriadData_<_T>> &samples1,
+                             TimestampUnit unit, DatasetType type) {
   samples0.clear();
   samples1.clear();
 
@@ -95,35 +88,32 @@ void imu_tk::importAsciiData ( const char *filename,
   ifstream infile;
   double ts, d[6];
 
-  infile.open ( filename );
-  if ( infile.is_open() )
-  {
+  infile.open(filename);
+  if (infile.is_open()) {
     char format[266];
-    switch ( type )
-    {
+    switch (type) {
     case DATASET_COMMA_SEPARATED:
-      sprintf ( format,"%%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf" );
+      sprintf(format, "%%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf");
       break;
     case DATASET_SPACE_SEPARATED:
     default:
-      sprintf ( format,"%%lf %%lf %%lf %%lf %%lf %%lf %%lf" );
+      sprintf(format, "%%lf %%lf %%lf %%lf %%lf %%lf %%lf");
       break;
     }
 
     int l = 0;
-    while ( getline ( infile,line ) )
-    {
-      int res = sscanf ( line.data(), format, &ts, &d[0], &d[1], &d[2],
-                         &d[3], &d[4], &d[5] );
-      if ( res != 7 )
-      {
-        cout<<"importAsciiData(): error importing data in line "<<l<<", exit"<<endl;
-      }
-      else
-      {
+    while (getline(infile, line)) {
+      int res = sscanf(line.data(), format, &ts, &d[0], &d[1], &d[2], &d[3],
+                       &d[4], &d[5]);
+      if (res != 7) {
+        cout << "importAsciiData(): error importing data in line " << l
+             << ", exit" << endl;
+      } else {
         ts /= unit;
-        samples0.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[0] ), _T ( d[1] ), _T ( d[2] ) ) );
-        samples1.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[3] ), _T ( d[4] ), _T ( d[5] ) ) );
+        samples0.push_back(
+            TriadData_<_T>(_T(ts), _T(d[0]), _T(d[1]), _T(d[2])));
+        samples1.push_back(
+            TriadData_<_T>(_T(ts), _T(d[3]), _T(d[4]), _T(d[5])));
       }
       l++;
     }
@@ -132,12 +122,11 @@ void imu_tk::importAsciiData ( const char *filename,
 }
 
 template <typename _T>
-void imu_tk::importAsciiData ( const char *filename,
-                               vector< TriadData_<_T> > &samples0,
-                               vector< TriadData_<_T> > &samples1,
-                               vector< TriadData_<_T> > &samples2,
-                               TimestampUnit unit, DatasetType type )
-{
+void imu_tk::importAsciiData(const char *filename,
+                             vector<TriadData_<_T>> &samples0,
+                             vector<TriadData_<_T>> &samples1,
+                             vector<TriadData_<_T>> &samples2,
+                             TimestampUnit unit, DatasetType type) {
   samples0.clear();
   samples1.clear();
   samples2.clear();
@@ -146,36 +135,35 @@ void imu_tk::importAsciiData ( const char *filename,
   ifstream infile;
   double ts, d[9];
 
-  infile.open ( filename );
-  if ( infile.is_open() )
-  {
+  infile.open(filename);
+  if (infile.is_open()) {
     char format[266];
-    switch ( type )
-    {
+    switch (type) {
     case DATASET_COMMA_SEPARATED:
-      sprintf ( format,"%%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf" );
+      sprintf(format,
+              "%%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf, %%lf");
       break;
     case DATASET_SPACE_SEPARATED:
     default:
-      sprintf ( format,"%%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf" );
+      sprintf(format, "%%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf %%lf");
       break;
     }
 
     int l = 0;
-    while ( getline ( infile,line ) )
-    {
-      int res = sscanf ( line.data(), format, &ts, &d[0], &d[1], &d[2],
-                         &d[3], &d[4], &d[5], &d[6], &d[7], &d[8] );
-      if ( res != 10 )
-      {
-        cout<<"importAsciiData(): error importing data in line "<<l<<", exit"<<endl;
-      }
-      else
-      {
+    while (getline(infile, line)) {
+      int res = sscanf(line.data(), format, &ts, &d[0], &d[1], &d[2], &d[3],
+                       &d[4], &d[5], &d[6], &d[7], &d[8]);
+      if (res != 10) {
+        cout << "importAsciiData(): error importing data in line " << l
+             << ", exit" << endl;
+      } else {
         ts /= unit;
-        samples0.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[0] ), _T ( d[1] ), _T ( d[2] ) ) );
-        samples1.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[3] ), _T ( d[4] ), _T ( d[5] ) ) );
-        samples2.push_back ( TriadData_<_T> ( _T ( ts ), _T ( d[6] ), _T ( d[7] ), _T ( d[8] ) ) );
+        samples0.push_back(
+            TriadData_<_T>(_T(ts), _T(d[0]), _T(d[1]), _T(d[2])));
+        samples1.push_back(
+            TriadData_<_T>(_T(ts), _T(d[3]), _T(d[4]), _T(d[5])));
+        samples2.push_back(
+            TriadData_<_T>(_T(ts), _T(d[6]), _T(d[7]), _T(d[8])));
       }
       l++;
     }
@@ -183,27 +171,25 @@ void imu_tk::importAsciiData ( const char *filename,
   }
 }
 
-template void imu_tk::importAsciiData<double> ( const char *filename,
-    vector< TriadData_<double> > &samples,
-    TimestampUnit unit, DatasetType type );
-template void imu_tk::importAsciiData<float> ( const char *filename,
-    vector< TriadData_<float> > &samples,
-    TimestampUnit unit, DatasetType type );
-template void imu_tk::importAsciiData<double> ( const char *filename,
-    vector< TriadData_<double> > &samples0,
-    vector< TriadData_<double> > &samples1,
-    TimestampUnit unit, DatasetType type );
-template void imu_tk::importAsciiData<float> ( const char *filename,
-    vector< TriadData_<float> > &samples0,
-    vector< TriadData_<float> > &samples1,
-    TimestampUnit unit, DatasetType type );
-template void imu_tk::importAsciiData<double> ( const char *filename,
-    vector< TriadData_<double> > &samples0,
-    vector< TriadData_<double> > &samples1,
-    vector< TriadData_<double> > &samples2,
-    TimestampUnit unit, DatasetType type );
-template void imu_tk::importAsciiData<float> ( const char *filename,
-    vector< TriadData_<float> > &samples0,
-    vector< TriadData_<float> > &samples1,
-    vector< TriadData_<float> > &samples2,
-    TimestampUnit unit, DatasetType type );
+template void
+imu_tk::importAsciiData<double>(const char *filename,
+                                vector<TriadData_<double>> &samples,
+                                TimestampUnit unit, DatasetType type);
+template void imu_tk::importAsciiData<float>(const char *filename,
+                                             vector<TriadData_<float>> &samples,
+                                             TimestampUnit unit,
+                                             DatasetType type);
+template void imu_tk::importAsciiData<double>(
+    const char *filename, vector<TriadData_<double>> &samples0,
+    vector<TriadData_<double>> &samples1, TimestampUnit unit, DatasetType type);
+template void imu_tk::importAsciiData<float>(
+    const char *filename, vector<TriadData_<float>> &samples0,
+    vector<TriadData_<float>> &samples1, TimestampUnit unit, DatasetType type);
+template void imu_tk::importAsciiData<double>(
+    const char *filename, vector<TriadData_<double>> &samples0,
+    vector<TriadData_<double>> &samples1, vector<TriadData_<double>> &samples2,
+    TimestampUnit unit, DatasetType type);
+template void imu_tk::importAsciiData<float>(
+    const char *filename, vector<TriadData_<float>> &samples0,
+    vector<TriadData_<float>> &samples1, vector<TriadData_<float>> &samples2,
+    TimestampUnit unit, DatasetType type);
